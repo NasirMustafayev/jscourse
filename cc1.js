ec373de6ebdc039e4479accbb541c9c64f02c204
Nasir Mustafayev <nasir.mustafayev@gmail.com>
2022-10-23 19:54:20 +0400

    First

---
+++ cc1.js
@@ -0,0 +1,11 @@
+let weightMark = 78;
+let weightJohn = 92;
+let heightMark = 1.69;
+let heightJohn = 1.95;
+
+let bmiMark = weightMark / heightMark ** 2;
+let bmiJohn = weightJohn / heightJohn ** 2;
+
+console.log(bmiMark + " " + bmiJohn);
+
+console.log(bmiMark > bmiJohn);
