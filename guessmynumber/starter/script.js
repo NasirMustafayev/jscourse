099ff0ad4920e4be907cac67d1f1ae9a4b0475ca
Nasir Mustafayev <nasir.mustafafayev@gmail.com>
2022-11-04 02:07:26 +0400

    Nineth commit

--- guessmynumber/starter/script.js
+++ guessmynumber/starter/script.js
@@ -1,22 +1,45 @@
 "use strict";
 
-// document.querySelector(".number").textContent = "?";
-// document.querySelector(".guess").value = "0";
-
 if (document.querySelector(".highscore").textContent == 0) {
   document.querySelector(".label-highscore").textContent =
     "🤷‍♂️ Not any score yet";
 }
 
 const secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+const msgBox = (value) => {
+  return (document.querySelector(".message").textContent = value);
+};
+const scoreBox = (value) => {
+  return (document.querySelector(".score").textContent = value);
+};
+
+////////////////////////////////////////////////////////////////
 
 document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
 
-  if (guess && guess === secretNum) {
-    console.log("Guessed!");
-
-    document.querySelector(".message").textContent =
-      "🥳 Winner Winner 🍗 Chicken Dinner!";
+  if (guess === secretNum && score > 1) {
+    msgBox("🥳 Winner Winner 🍗Chicken Dinner!");
+  } else if (!guess) {
+    msgBox("❌Type a valid number!");
+  } else if (
+    (guess + 1 === secretNum && score > 1) ||
+    (guess - 1 === secretNum && score > 1)
+  ) {
+    msgBox("🙈 Near");
+    score--;
+    scoreBox(score);
+  } else if (guess > secretNum && score > 1) {
+    msgBox("📈 Too high");
+    score--;
+    scoreBox(score);
+  } else if (guess < secretNum && score > 1) {
+    msgBox("📉 Too low");
+    score--;
+    scoreBox(score);
+  } else {
+    msgBox("💢 You lose!");
+    scoreBox(0);
   }
 });
