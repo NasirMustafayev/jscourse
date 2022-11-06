5d4dc09f84c08ab89a6e83059b4bbde06dbb4ef2
Nasir Mustafayev <nasir.mustafayev@gmail.com>
2022-11-07 01:24:28 +0400

    Thirteenth commit modal Refactoring

--- modal/starter/script.js
+++ modal/starter/script.js
@@ -6,20 +6,22 @@ const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
+//Functions for not repeatig and not messy looking codes
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
 //Starting loop for interacting with all buttons
 for (let i = 0; i < modalBtns.length; i++) {
-  modalBtns[i].addEventListener("click", () => {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  modalBtns[i].addEventListener("click", openModal);
 }
+
 //Clicking close button
-closeBtn.addEventListener("click", () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+closeBtn.addEventListener("click", closeModal);
 //When you click in outside(overlay) of modal
-overlay.addEventListener("click", () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+overlay.addEventListener("click", closeModal);
