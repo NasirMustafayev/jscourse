e1069f923220754b0535807d03db3cadcad2d914
Nasir Mustafayev <nasir.mustafafayev@gmail.com>
2022-11-23 01:09:18 +0400

    Twentyth commit

--- datastructures/cc1.js
+++ datastructures/cc1.js
@@ -38,3 +38,15 @@ const game = {
     team2: 6.5,
   },
 };
+
+const [players1, players2] = game.players;
+
+const [gk, ...fieldPlayers] = players1;
+
+const allPlayers = [...players1, ...players2];
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+const { team1, x: draw, team2 } = game.odds;
+
+function printGoals(...params) {}
