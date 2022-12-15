ccb152174e5edc7fbd42ec6c6e4ee8538d87cdbd
Nasir Mustafayev <nasir.mustafafayev@gmail.com>
2022-12-15 23:57:44 +0400

    Thirtysecond commit functions cc1 second

--- functions/cc1.js
+++ functions/cc1.js
@@ -2,9 +2,24 @@ const poll = {
   question: "What is your favourite programming language?",
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
+  displayResults(type = "array") {
+    if (typeof type == "string") {
+      const [...points] = this.answers;
+      console.log(`Poll results are ${points.toString()}`);
+    } else {
+      console.log(this.answers);
+    }
+  },
   registerNewAnswer() {
-    const answer = prompt(`${this.question}
-    ${this.options}`);
+    const answer = Number(
+      prompt(`${this.question}
+    ${this.options}`)
+    );
+
+    answer <= 3
+      ? this.answers[answer]++
+      : console.log("Select a option from list");
+    this.displayResults("string");
   },
 };
 
