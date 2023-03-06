f6a3ce971532282edfae34094a584197fb387e86
Nasir <nasir.mustafayev@gmail.com>
2023-03-06 09:34:22 +0400

    Sixtythirdth commit Mapty add Leaflet getlocation

--- mapty/script.js
+++ mapty/script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition((location) => {
+        const { latitude, longitude } = location.coords;
+        console.log(location);
+
+
+        const map = L.map('map').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+    }, () => {
+        alert("Can't get your current location there is a problem");
+    })
