96001d9be645958ccdc2c70047ad0d18f8c61428
Nasir <nasir.mustafayev@gmail.com>
2023-03-10 09:48:45 +0400

    Sixtyfourth commit Mapty Show submit form on click, Change running to cycling in select

--- mapty/script.js
+++ mapty/script.js
@@ -11,21 +11,46 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition((location) => {
         const { latitude, longitude } = location.coords;
-        console.log(location);
 
-
-        const map = L.map('map').setView([latitude, longitude], 13);
+        map = L.map('map').setView([latitude, longitude], 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([latitude, longitude]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+        map.on("click", (mapE) => {
+            mapEvent = mapE;
+            form.classList.remove("hidden")
+            inputDistance.focus();
+        })
+
     }, () => {
         alert("Can't get your current location there is a problem");
     })
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+
+    const { lat, lng } = mapEvent.latlng
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup"
+        }))
+        .setPopupContent("Wata do doin??")
+        .openPopup();
+})
+inputType.addEventListener("change", () => {
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+})
