6207432cffa5ded906b1a93a1b86e630c18e6eda
Nasir <nasir.mustafayev@gmail.com>
2023-03-12 23:44:08 +0400

    Sixtysixth commit Mapty app Workout classes initial structure written

--- mapty/script.js
+++ mapty/script.js
@@ -10,7 +10,39 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+    date = new Date();
+
+    constructor(coords, distance, duration) {
+        this.coords = coords;
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        this.pace = this.duration / this.distance
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60);
+    }
+}
 
 class App {
     #map;
