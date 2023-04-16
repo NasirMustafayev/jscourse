72b59cc66d1e05dd6ee718c0e06ea085c7cc7aa9
Nasir <nasir.mustafayev@gmail.com>
2023-04-16 15:16:39 +0400

    Sevetyoneth commit Mapty app adding Reset button and modal window

--- mapty/script.js
+++ mapty/script.js
@@ -9,6 +9,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const btnReset = document.querySelector(".btn__reset");
+const btnSort = document.querySelector(".btn__sort");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnYes = document.querySelector(".btn__yes");
+const btnNo = document.querySelector(".btn__no");
+
+let tabActions;
+let btnsEdit;
+let btnsDelete;
 
 class Workout {
     date = new Date();
@@ -62,10 +73,22 @@ class App {
 
     constructor() {
         this._getPosition();
+
         form.addEventListener("submit", this._newWorkout.bind(this));
         inputType.addEventListener("change", this._toggleElevationField);
+
         containerWorkouts.addEventListener("click", this._moveToTarget.bind(this));
+
         this._getFromLocalStorage();
+        btnReset.addEventListener("click", this.openModal);
+        // btnSort.addEventListener("click", this.sort.bind(this));
+
+        document.addEventListener("keydown", this.KeydownHidden.bind(this));
+        btnCloseModal.addEventListener("click", this.closeModal);
+        btnYes.addEventListener("click", this.reset);
+        btnNo.addEventListener("click", this.closeModal);
+
+        this._removeResetBtn();
     }
 
     _getPosition() {
@@ -105,6 +128,9 @@ class App {
 
     _newWorkout(event) {
         event.preventDefault();
+
+        //Reset button make visible again
+        this._addResetBtn();
 
         //Input validation functionn
         const validValue = (...values) =>
@@ -202,6 +228,7 @@ class App {
                 <span class="workout__value">${workout.pace.toFixed(1)}</span>
                 <span class="workout__unit">spm</span>
             </div>
+            <div class="workout__actions"><a href="" id="edit">✏️</a> | <a href="" id="delete">🗑️</a></div>
         </li>`;
 
         if (workout.type === "cycling")
@@ -216,13 +243,22 @@ class App {
                 <span class="workout__value">${workout.elevationGain}</span>
                 <span class="workout__unit">m</span>
             </div>
+            <div class="workout__actions"><a href="" id="edit">✏️</a> | <a href="" id="delete">🗑️</a></div>
         </li>
         `;
 
         form.insertAdjacentHTML("afterend", html);
 
+        //Doesn't work
+        // tabActions = document.querySelector(".workout__actions");
+        // btnsEdit = document.querySelectorAll("#edit");
+        // btnsDelete = document.querySelectorAll("#delete");
+
+        // btnsDelete.forEach(btn => btn.addEventListener("click", this._deleteWorkout.bind(this)));
+
     }
 
+    //Move to target location on map when clicked on list
     _moveToTarget(e) {
         const workoutEl = e.target.closest(".workout")
 
@@ -238,9 +274,12 @@ class App {
         })
     }
 
+    //Setting workout data in local storage
     _setToLocalStorage() {
         localStorage.setItem("workouts", JSON.stringify(this.#workouts));
     }
+
+    //Getting workout data from local storage
     _getFromLocalStorage() {
         const localdata = JSON.parse(localStorage.getItem("workouts"));
 
@@ -254,10 +293,68 @@ class App {
 
     }
 
+    //Removing "Clear all" button when workout list empty
+    _removeResetBtn() {
+        if (this.#workouts.length === 0)
+            btnReset.classList.add("hidden");
+    }
+    //Add "Clear all" button as soon as there is data on list
+    _addResetBtn() {
+        if (this.#workouts.length > 0)
+            btnReset.classList.remove("hidden");
+    }
+    //Doesn't work
+    // _deleteWorkout(e) {
+    //     e.preventDefault();
+    //     const workoutEl = e.target.closest(".workout");
+
+    //     const localdata = JSON.parse(localStorage.getItem("workouts"));
+    //     //When we delete an object from local storage they reorderded again by automatic index number so thats why hard to catch them
+    //     const itemindex = localdata.find(item => item.id === workoutEl.dataset.id);
+    //     localdata.splice(itemindex, 1);
+    //     localStorage.setItem("workouts", JSON.stringify(localdata));
+
+    //     workoutEl.remove();
+    //     this._removeResetBtn();
+    // }
+
+    //Open confirmation modal window about wipe out all data
+    openModal(e) {
+        e.preventDefault();
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    }
+
+    //Close modal window
+    closeModal() {
+        modal.classList.add("hidden");
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+    }
+
+    //Close modal window when pressing "Esc" key
+    KeydownHidden(e) {
+        if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+            this.closeModal();
+        }
+    };
+
+    //Clearing all Workout data from storage
     reset() {
         localStorage.removeItem("workouts");
         location.reload();
     }
+
+    //Doesn't work
+    // sort() {
+    //     //Sort by distance
+
+    //     const sorting = this.#workouts.slice().sort((a, b) => a - b);
+    //     console.log(sorting);
+
+    //     this._renderWorkout(sorting);
+
+    // }
 }
 
 const app = new App();
