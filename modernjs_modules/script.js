97a8361f71eabf964557e239be96b808edf5fbb1
Nasir Mustafayev <nasir.mustafayev@gmail.com>
2023-05-13 13:00:46 +0400

    Revert "Eighty fifth commit | NPM init and install, Bundling with Parcel and examples"
    This reverts commit a45e51251754c0337bc88cfa96746b552e7cf9ae.

--- modernjs_modules/script.js
+++ modernjs_modules/script.js
@@ -3,15 +3,9 @@
 // addMember("Nasir", "Mustafayev");
 // console.log(members);
 
-import deepClone from "lodash-es";
 import * as User from "./module1.js"
 
 
 User.addMember("Nasir", "Mustafayev");
 console.log(User.members);
 console.log(User);
-console.log(deepClone);
-
-if (module.hot) {
-    module.hot.accept();
-}
